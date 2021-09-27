848abe8a72597465480a882f73f7780b25c82874
bhatia-di <bhatia.di@northeastern.edu>
2021-09-26 23:28:49 -0400

    Add Insertion sort benchmark sorted array output

--- src/main/java/edu/neu/coe/info6205/benchmark/InsertionSortBenchmark.java
+++ src/main/java/edu/neu/coe/info6205/benchmark/InsertionSortBenchmark.java
@@ -17,21 +17,71 @@ public class InsertionSortBenchmark {
         Integer[] listInt;
         Benchmark<Integer[]> benchmark = new Benchmark_Timer<Integer[]>("SystemSort", null, InsertionSort::sort, null);
         final double time = benchmark.runFromSupplier(() -> Utilities.fillRandomArray(Integer.class, random, numberOfintergers, r -> r.nextInt()), nRuns);
-        System.out.println(numberOfintergers + "     |" + time);
+        System.out.println(numberOfintergers + "     " + time);
 
+    }
+    public void testInsertionSortForReversedArray(int numberOfintergers) throws Exception {
+        int nRuns = 10;
+        Random random = new Random();
+        Integer[] listInt;
+        Benchmark<Integer[]> benchmark = new Benchmark_Timer<Integer[]>("SystemSort", null, InsertionSort::sort, null);
+        final double time = benchmark.runFromSupplier(() -> createReversedArray(numberOfintergers), nRuns);
+        System.out.println(numberOfintergers + "     " + time);
+
+    }
+    public void testInsertionSortForSortedArray(int numberOfintergers) throws Exception {
+        int nRuns = 10;
+        Random random = new Random();
+        Integer[] listInt;
+        Benchmark<Integer[]> benchmark = new Benchmark_Timer<Integer[]>("SystemSort", null, InsertionSort::sort, null);
+        final double time = benchmark.runFromSupplier(() -> createSortedArray(numberOfintergers), nRuns);
+        System.out.println(numberOfintergers + "     " + time);
+
+    }
+    public Integer[] createSortedArray(int noOfIntegers) {
+        Integer[] result = new Integer[noOfIntegers];
+        for (int index = 0; index< noOfIntegers; index++) {
+            result [index] = index;
+        }
+        return result;
+    }
+    public Integer[] createReversedArray(int noOfIntegers) {
+        Integer[] result = new Integer[noOfIntegers];
+        for (int index = 0,value=noOfIntegers; index< noOfIntegers; index++,value--) {
+            result [index] = value;
+        }
+        return result;
     }
 
    public static void main(String[] args) throws Exception {
-        InsertionSortBenchmark insertionSortBenchmark = new InsertionSortBenchmark();
-        int numberOfTrials = 30;
-        int startNumberOfIntegers  = 4;
-        System.out.println("Number Of Integer     |" + "Mean Time");
-        for (int index= 0; index< numberOfTrials; index++) {
-            insertionSortBenchmark.testInsertionSortForRandomArray(startNumberOfIntegers);
-            startNumberOfIntegers = startNumberOfIntegers*2;
+       InsertionSortBenchmark insertionSortBenchmark = new InsertionSortBenchmark();
+       int numberOfTrials = 12;
+       int startNumberOfIntegers = 4;
+//       System.out.println("RANDOM ARRAY");
+//       System.out.println("Number Of Integer     " + "Mean Time");
+//       for (int index = 0; index < numberOfTrials; index++) {
+//           insertionSortBenchmark.testInsertionSortForRandomArray(startNumberOfIntegers);
+//           startNumberOfIntegers = startNumberOfIntegers * 2;
+//
+//       }
 
-        }
+//       System.out.println("REVERSED ARRAY");
+//       System.out.println("Number Of Integer     |" + "Mean Time");
+//       for (int index = 0; index < numberOfTrials; index++) {
+//           insertionSortBenchmark.testInsertionSortForReversedArray(startNumberOfIntegers);
+//           startNumberOfIntegers = startNumberOfIntegers * 2;
+//
+//       }
+
+       System.out.println("SORTED ARRAY");
+       System.out.println("Number Of Integer     |" + "Mean Time");
+       for (int index = 0; index < numberOfTrials; index++) {
+           insertionSortBenchmark.testInsertionSortForSortedArray(startNumberOfIntegers);
+           startNumberOfIntegers = startNumberOfIntegers * 2;
+
+       }
    }
+
 
 
 }
