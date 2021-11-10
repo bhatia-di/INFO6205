793f449bd3a1c8e5ad2baeb9522eb855d96ba486
bhatia-di <bhatia.di@northeastern.edu>
2021-11-10 14:25:28 -0500

    Added python file to get min

--- src/main/java/edu/neu/coe/info6205/assign_5_results/PlotMin.py
+++ src/main/java/edu/neu/coe/info6205/assign_5_results/PlotMin.py
@@ -1,13 +1,10 @@
 import pandas as pd
+import matplotlib
 
-try:
-    import numpy
-except ImportError:
-    print "numpy is not installed"
 
 df1 = pd.read_csv("./result_thread_count_2_arraySize_2000000.csv", index_col=None, header=0, names=['x', 'y'])
 df2 = pd.read_csv("./result_thread_count_2_arraySize_3000000.csv", index_col=None, header=0, names=['x', 'y'])
 
 
-ax = df1.plot(figsize=(20,10))
+ax = df1.plot()
 df2.plot(ax=ax)
